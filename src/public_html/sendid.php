db4c271f9b9438cffcf51b56b725d82a11be6898
Diego Feitosa <dnfeitosa@gmail.com>
2012-01-04 20:22:57 -0200

    moving php file that was on whiteboard project

---
+++ src/public_html/sendid.php
@@ -0,0 +1,11 @@
+<?php
+require_once dirname(dirname( __FILE__ )) . '/lib/vendor/symfony1.2/lib/autoload/sfCoreAutoload.class.php';
+require_once dirname(dirname( __FILE__ )) . '/lib/RaykuCommon.class.php';
+sfCoreAutoload::register();
+
+$connection = RaykuCommon::getDatabaseConnection();
+$time = time();
+
+mysql_query("INSERT INTO `sendmessage` (`expert_id`, `asker_id`, `chat_id`, `time`) VALUES('".$_REQUEST['loginId']."', '".$_REQUEST['askerid']."', '".$_REQUEST['newchatid']."', '".$time."')", $connection) or die(mysql_error());
+mysql_query("insert into popup_close(user_id) values(".$_REQUEST['loginId'].")", $connection) or die(mysql_error());
+?>
