ab86274f6ceb4fb25fe822576bb21a7c2cffd39f
Łukasz Wojciechowski <lukasz.wojciechowski@ngsoft.pl>
2012-03-04 12:16:29 +0100

    got rid of 'prod2' environment

--- src/public_html/index.php
+++ src/public_html/index.php
@@ -4,5 +4,5 @@ require_once(dirname(__FILE__).'/../config/ProjectConfiguration.class.php');
 require_once( dirname( __FILE__ ) . '/classroomAppDetector.php' );
 $app = classroomAppDetector( $_SERVER['REQUEST_URI'], 'frontend' );
 
-$configuration = ProjectConfiguration::getApplicationConfiguration($app, 'prod2', false);
+$configuration = ProjectConfiguration::getApplicationConfiguration($app, 'prod', false);
 sfContext::createInstance($configuration)->dispatch();
