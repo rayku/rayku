8a14021b181dfbd26bf0477e1970fb5d9431a943
Łukasz Wojciechowski <lukasz.wojciechowski@ngsoft.pl>
2012-03-09 13:41:21 +0100

    simplified UsersAvailabilityChecker code

--- src/lib/service/UsersAvailabilityChecker.class.php
+++ src/lib/service/UsersAvailabilityChecker.class.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The job of this class is to check current online/offline status of users
  *
@@ -9,103 +10,67 @@ class UsersAvailabilityChecker
 
     function getOnlineUsersCount()
     {
-
         $c = new Criteria();
-        $newUser = UserPeer::doSelect($c);
+        $allUsers = UserPeer::doSelect($c);
 
         $onlineusers = array();
-        $offlineusers = array();
-        $newOnlineUser = array();
-        $newOfflineUser = array();
 
-        $j = 0;
-        $k = 0;
         $facebookTutors = BotServiceProvider::createFor("http://facebook.rayku.com/tutor")->getContent();
         $onlineTutorsByNotificationBot = BotServiceProvider::createFor("http://notification-bot.rayku.com/tutor")->getContent();
 
-        $Users = json_decode($facebookTutors, true);
-        $_Users = json_decode($onlineTutorsByNotificationBot, true);
+        $fbUsers = json_decode($facebookTutors, true);
+        $botUsers = json_decode($onlineTutorsByNotificationBot, true);
 
 
-        foreach ($newUser as $new):
-
-
-            $a = new Criteria();
-            $a->add(UserPeer::ID, $new->getId());
-            $users_online = UserPeer::doSelectOne($a);
-
-            $onlinecheck = '';
-
-            if ($users_online->isOnline()) {
-
-                $onlinecheck = "online";
+        /* @var $user User */
+        foreach ($allUsers as $user) {
+            if ($user->isOnline()) {
+                $onlineusers[] = $user->getId();
+                continue;
             }
 
-            if (empty($onlinecheck)) {
+            $gtalkquery = mysql_query("select * from user_gtalk where userid=" . $user->getId()) or die(mysql_error());
 
-                $gtalkquery = mysql_query("select * from user_gtalk where userid=" . $new->getId()) or die(mysql_error());
+            if (mysql_num_rows($gtalkquery) > 0) {
 
-                if (mysql_num_rows($gtalkquery) > 0) {
+                $status = mysql_fetch_assoc($gtalkquery);
 
-                    $status = mysql_fetch_assoc($gtalkquery);
+                $gtalkmail = $status['gtalkid'];
 
-                    $gtalkmail = $status['gtalkid'];
-
-                    $onlinecheck = BotServiceProvider::createFor('http://www.rayku.com:8892/status/' . $gtalkmail)->getContent();
+                $onlinecheck = BotServiceProvider::createFor('http://www.rayku.com:8892/status/' . $gtalkmail)->getContent();
+                if ($onlinecheck == 'online') {
+                    $onlineusers[] = $user->getId();
+                    continue;
                 }
             }
 
-            if ((empty($onlinecheck) || ($onlinecheck != "online")) && is_array($Users)) {
 
-                $fb_query = mysql_query("select * from user_fb where userid=" . $new->getId()) or die(mysql_error());
+            if (is_array($fbUsers)) {
+                $fb_query = mysql_query("select * from user_fb where userid=" . $user->getId()) or die(mysql_error());
 
                 if (mysql_num_rows($fb_query) > 0) {
 
                     $fbRow = mysql_fetch_assoc($fb_query);
-
                     $fb_username = $fbRow['fb_username'];
 
-
-                    foreach ($Users as $key => $user) :
-
-                        if ($user['username'] == $fb_username):
-
-                            $onlinecheck = 'online';
-
-                            break;
-                        endif;
-
-                    endforeach;
+                    foreach ($fbUsers as $key => $fbUser) {
+                        if ($fbUser['username'] == $fb_username) {
+                            $onlineusers[] = $user->getId();
+                            continue 2;
+                        }
+                    }
                 }
             }
 
-            if ((empty($onlinecheck) || ($onlinecheck != "online")) && is_array($_Users)) {
-
-
-
-                foreach ($_Users as $key => $_user) :
-
-                    if ($_user['email'] == $users_online->getEmail()):
-
-                        $onlinecheck = 'online';
-                        break;
-                    endif;
-
-                endforeach;
+            if (is_array($botUsers)) {
+                foreach ($botUsers as $key => $botUser) {
+                    if ($botUser['email'] == $user->getEmail()) {
+                        $onlineusers[] = $user->getId();
+                        continue 2;
+                    }
+                }
             }
-
-
-
-            if ($onlinecheck == "online") {
-
-                $onlineusers[$j] = $new->getId();
-                $j++;
-            } elseif ($users_online->isOnline()) {
-                $onlineusers[$j] = $new->getId();
-                $j++;
-            }
-
-        endforeach;
+        }
 
         return count($onlineusers);
     }
