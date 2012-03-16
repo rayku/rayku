50dfb27b2950f3db3e1690651e4adc68f93ba7b3
Diego Feitosa <dnfeitosa@gmail.com>
2012-03-16 12:08:15 -0300

    adding whiteboard connection table

---
+++ db/001-create-whiteboard-connections.sql
@@ -0,0 +1,20 @@
+alter table student_questions engine innodb;
+
+CREATE TABLE `whiteboard_connections`
+(
+    `id` INTEGER(11)  NOT NULL AUTO_INCREMENT,
+    `question_id` INTEGER(11)  NOT NULL,
+    `type` INTEGER(10)  NOT NULL,
+    `token` VARCHAR(40)  NOT NULL,
+    `user_id` INTEGER(11)  NOT NULL,
+    PRIMARY KEY (`id`),
+    INDEX `whiteboard_connections_FI_1` (`question_id`),
+    CONSTRAINT `whiteboard_connections_FK_1`
+        FOREIGN KEY (`question_id`)
+        REFERENCES `student_questions` (`id`),
+    INDEX `whiteboard_connections_FI_2` (`user_id`),
+    CONSTRAINT `whiteboard_connections_FK_2`
+        FOREIGN KEY (`user_id`)
+        REFERENCES `user` (`id`)
+)Type=InnoDB;
+
