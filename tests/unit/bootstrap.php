3c6a70590a81b12106eb97715607a2189fb04a78
Diego Feitosa <dnfeitosa@gmail.com>
2012-03-18 16:04:41 -0300

    adding setup for unit tests

---
+++ tests/unit/bootstrap.php
@@ -0,0 +1,21 @@
+<?php
+require_once dirname(__FILE__) . '/../../src/lib/vendor/phake/src/Phake.php';
+require_once dirname(__FILE__) . '/../../src/lib/vendor/symfony1/lib/autoload/sfCoreAutoload.class.php';
+require_once dirname(__FILE__) . '/../../src/config/ProjectConfiguration.class.php';
+
+function when($object)
+{
+    return Phake::when($object);
+}
+
+function mock($className)
+{
+    return Phake::mock($className);
+}
+
+$autoloader = sfCoreAutoload::getInstance();
+$autoloader->register();
+
+$configuration = ProjectConfiguration::getApplicationConfiguration('frontend', 'unit-test', true);
+sfContext::createInstance($configuration);
+
