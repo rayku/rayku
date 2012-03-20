d7d6d8cd90fa20734530711afa1fcca56146358a
Diego Feitosa <dnfeitosa@gmail.com>
2012-03-20 01:15:35 -0300

    moving phpunit.xml file to a proper place + readding database configuration to bootstrap

--- tests/unit/bootstrap.php
+++ tests/unit/bootstrap.php
@@ -19,3 +19,15 @@ $autoloader->register();
 $configuration = ProjectConfiguration::getApplicationConfiguration('frontend', 'unit-test', true);
 sfContext::createInstance($configuration);
 
+chdir(dirname(__FILE__) . '/../../src/');
+$dispatcher = new sfEventDispatcher();
+$application = new sfSymfonyCommandApplication(
+    $dispatcher,
+    new sfAnsiColorFormatter(),
+    array(
+        'symfony_lib_dir' => realpath(dirname(__FILE__).'/../..'),
+        'sf_root_dir' => realpath(dirname(__FILE__).'/../../src')
+    )
+);
+$application->run(array('propel:build-sql'));
+$application->run(array('propel:insert-sql', '--env="unit-tests"', '--no-confirmation'));
