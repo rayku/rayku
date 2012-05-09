5e056f2639762c3d0b5c48ab17699384cdd0590b
Łukasz Wojciechowski <lukasz.wojciechowski@ngsoft.pl>
2012-05-09 10:50:13 +0200

    few files deleted on production by Donny

--- src/apps/frontend/modules/register/templates/_confirmationEmail.php
+++
@@ -1,9 +0,0 @@
-Hello <?php echo $user->getName(); ?>
-
-Welcome to Rayku.com!
-
-Your account has been activated. You may now login to your Rayku.com account
-using the email and password entered during the registration process.
-
-
-<?php include_partial( 'global/emailFooter' ); ?>
