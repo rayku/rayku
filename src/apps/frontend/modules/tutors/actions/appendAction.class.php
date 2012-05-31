c9b505d4e3c4f82e75d9e217a1995e678d4781d0
Łukasz Wojciechowski <lukasz.wojciechowski@ngsoft.pl>
2012-05-31 21:02:01 +0200

    extracted tutors/append action into separate file

--- src/apps/frontend/modules/tutors/actions/appendAction.class.php
+++ src/apps/frontend/modules/tutors/actions/appendAction.class.php
@@ -7,19 +7,13 @@
  * @subpackage friends
  * @author     Adam A Flynn <adamaflynn@criticaldevelopment.net>
  */
-class tutorsActions extends sfActions {
+class appendAction extends sfAction
+{
 
-    public function preExecute()
+    public function execute($request)
     {
         RaykuCommon::getDatabaseConnection();
-    }
-
-    /**
-     * all members database
-     */
-    public function executeIndex()
-    {
-
+        
         $logedUserId = $_SESSION['symfony/user/sfUser/attributes']['symfony/user/sfUser/attributes']['user_id'];
 
         $currentUser = $this->getUser()->getRaykuUser();
@@ -29,27 +23,20 @@ class tutorsActions extends sfActions {
         $this->userId = $currentUser->getId();
 
 
-        /* Clearing Cookies 
-
-          if($_COOKIE['onoff'] != 1) {
-
-          for($u=$_COOKIE['cookcount'];$u>=1;$u--) {
-
-          $cookname =  'tutor_'.$u;
-
-          setcookie($cookname,'', time()-3600, "/");
-
-          }
-
-          setcookie("tutorcount",'', time()-3600, "/");
-          setcookie("cookcount",'', time()-3600, "/");
-
-          }
-
-          /* Clearing Cookies */
-
-
         $time = time();
+
+
+
+        /* if($this->getRequestParameter('rate') == 1) {		
+          $_SESSION["rateDisplay"] = 1;
+
+          } elseif($this->getRequestParameter('rate') == 2) {
+          unset($_SESSION["rateDisplay"]);
+          } else {
+          unset($_SESSION["rateDisplay"]);
+          } */
+
+        $_SESSION["rateDisplay"] = 1;
 
 
 
@@ -74,42 +61,22 @@ class tutorsActions extends sfActions {
             $this->getResponse()->setCookie("cookcount", '', time() - 3600, '/', sfConfig::get('app_cookies_domain'));
 
             /* Clearing Cookies */
-            if ($count == 4) {
-                $close = 46000;
-                $_SESSION['connected_tutors'] = 4;
-            }
-            if ($count == 3) {
 
-                $close = 46000;
-                $_SESSION['connected_tutors'] = 3;
+            if ($count > 2) {
+
+                $close = 21000;
             } else if ($count == 2) {
 
-                $close = 61000;
-                $_SESSION['connected_tutors'] = 2;
+                $close = 31000;
             } else if ($count == 1) {
 
                 $close = 61000;
-                $_SESSION['connected_tutors'] = 1;
-            } else {
-                $close = 61000;
-                $_SESSION['connected_tutors'] = 1;
             }
 
             $j = 0;
             for ($i = 0; $i < $count; $i++) {
 
                 mysql_query("INSERT INTO `user_expert` (`user_id`, `checked_id`, `category_id`, `question`, `exe_order`, `time`, status, close) VALUES ('" . $userId . "', '" . $_POST['checkbox'][$i] . "', '5', 'To be discussed','" . (++$j) . "', '" . $time . "', 1, " . $close . ") ") or die(mysql_error());
-            }
-
-            /* Notify same tutor again */
-
-            $l = 0;
-            $source = 'tutorlist';
-
-            mysql_query("DELETE FROM `student_questions` WHERE user_id=" . $userId . "");
-
-            for ($i = 0; $i < $count; $i++) {
-                mysql_query("INSERT INTO `student_questions` (`user_id`, `checked_id`, `category_id`, `question`, `exe_order`, `time`, status, close, source) VALUES ('" . $userId . "', '" . $_POST['checkbox'][$i] . "', '5', 'To be discussed','" . (++$l) . "', '" . $time . "', 1, " . $close . ", '" . $source . "') ") or die(mysql_error());
             }
 
 
@@ -130,7 +97,7 @@ class tutorsActions extends sfActions {
 
         $this->cat = $this->getRequestParameter('category');
 
-        $this->course_id = $this->getRequestParameter('course');
+        $this->course_id = $_SESSION['tutcourse']; //$this->getRequestParameter('course');
 
         if (empty($this->course_id)) {
             $this->course_id = 1;
@@ -174,6 +141,8 @@ class tutorsActions extends sfActions {
         foreach ($experts as $exp):
 
 
+            //if($userId != $exp->getUserId()):
+
             if (!in_array($exp->getUserId(), $newUserLimit)) :
 
                 $newUserLimit[] = $exp->getUserId();
@@ -181,15 +150,35 @@ class tutorsActions extends sfActions {
                 $_query = mysql_query("select * from user_tutor where userid =" . $exp->getUserId() . " ") or die(mysql_error());
                 if (mysql_num_rows($_query) > 0) :
 
-                    $_queryCourse = mysql_query("select * from expert_course where user_id =" . $exp->getUserId() . " and category_id = 1 and course_id = " . $this->course_id . " ") or die("Er-1-->" . mysql_error());
-                    if (mysql_num_rows($_queryCourse) > 0) :
+                    //$_queryCourse = mysql_query("select * from expert_course where user_id =".$exp->getUserId()." and category_id = 1 and course_id = ".$this->course_id." ") or die("Er-1-->".mysql_error()); 
+
+                    /* Student match with Tutors */
+                    $usrname = $this->getUser()->getRaykuUser()->getUsername();
+
+                    $_queryCourse = '';
+                    $tutorsq = mysql_query("select * from tutor_profile where category = 1 and user_id = " . $exp->getUserId() . "") or die("Er-1-->" . mysql_error());
+                    $tutors = mysql_fetch_array($tutorsq);
+                    $tutor = '';
+
+                    $tutor = explode("-", $tutors['course_id']);
+                    $coursid = $this->course_id;
+                    if (in_array($coursid, $tutor)) {
+                        //echo $coursid;
+
+                        $_queryCourse = mysql_query("select * from tutor_profile where category = 1 and user_id = " . $exp->getUserId() . "") or die("Er-1-->" . mysql_error());
+                        //echo "select * from tutor_profile where category = 1 and user_id = ".$exp->getUserId()."";
+                    }
+
+
+
+                    if ($_queryCourse && mysql_num_rows($_queryCourse) > 0) :
 
                         $query = mysql_query("select * from user_score where user_id=" . $exp->getUserId()) or die(mysql_error());
                         $score = mysql_fetch_assoc($query);
 
                         if ($score['score'] != 0):
 
-                            if (false) { //$_points == '' || $_points == '0.00'     Temporary hack
+                            if (false) { //$_points == '' || $_points == '0.00'   Temporary hack
                                 $emptyRCquery = mysql_query("select * from user_rate where userid=" . $exp->getUserId() . " and (rate = 0.00 || rate = 0) ") or die("Error In rate" . mysql_error());
 
                                 if (mysql_num_rows($emptyRCquery) > 0) {
@@ -219,6 +208,8 @@ class tutorsActions extends sfActions {
                 endif;
 
             endif;
+
+        //endif;	
 
 
         endforeach;
@@ -281,7 +272,6 @@ class tutorsActions extends sfActions {
 
                     $fb_username = $fbRow['fb_username'];
 
-
                     foreach ($facebookUsers as $key => $user) :
 
                         if ($user['username'] == $fb_username):
@@ -296,7 +286,6 @@ class tutorsActions extends sfActions {
             }
 
             if ((empty($onlinecheck) || ($onlinecheck != "online")) && is_array($botUsers)) {
-
 
                 foreach ($botUsers as $key => $_user) :
 
@@ -333,15 +322,14 @@ class tutorsActions extends sfActions {
 
         endforeach;
 
-
         $this->newOnlineUser = $newOnlineUser;
-
 
 
         $this->newOfflineUser = $newOfflineUser;
 
 
         $this->_checkOnlineUsers = $onlineusers;
+
 
 
 
@@ -434,43 +422,4 @@ class tutorsActions extends sfActions {
         $c->add(CategoryPeer::ID, $this->cat);
         $this->e = CategoryPeer::doSelectOne($c);
     }
-
-    public function executeAjaxidle()
-    {
-        if ($_GET['status'] == '1') {
-
-
-            $selmisqry = mysql_query("SELECT * FROM popup_close WHERE user_id ='" . $_GET['userid'] . "'");
-            $misqrys = mysql_fetch_array($selmisqry);
-
-            if ($misqrys['user_id'] == "") {
-                mysql_query("INSERT INTO `popup_close` (
-					 `id` ,
-					 `user_id`,
-					 `ustatus`
-					 )
-					 VALUES (
-					 NULL , '" . $_GET['userid'] . "','1' )");
-            }
-        } else if ($_GET['status'] == '2') {
-
-            $selmisqry = mysql_query("SELECT * FROM popup_close WHERE user_id ='" . $_GET['userid'] . "' and ustatus='1' ");
-            $misqrys = mysql_fetch_array($selmisqry);
-
-            if ($misqrys['user_id'] != "") {
-                $sel_misqry = mysql_query("DELETE FROM popup_close WHERE user_id='" . $_GET['userid'] . "'");
-            }
-        }
-    }
-
-    public function executeCheckoutpopup()
-    {
-        
-    }
-
-    public function executeDemopopup()
-    {
-        
-    }
-
 }
