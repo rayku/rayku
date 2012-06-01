39ab86b9fde2e109f2962403050bc4b80f3ad62a
Łukasz Wojciechowski <lukasz.wojciechowski@ngsoft.pl>
2012-06-01 05:55:57 +0200

    reduced over 3000 sql queries on /start page to just 2 :)

--- src/lib/service/UsersAvailabilityChecker.class.php
+++ src/lib/service/UsersAvailabilityChecker.class.php
@@ -7,56 +7,91 @@
  */
 class UsersAvailabilityChecker
 {
+    /**
+     * prefetched gtalk ids indexed by user id
+     */
+    private $gtalkIds = array();
+    
+    /**
+     * prefetched FB ids indexed by user id
+     */
+    private $fbIds = array();
+    
+    /**
+     * Simple array with all fb usernames currently being online
+     */
+    private $fbOnlineUsers = array();
+    
+    /**
+     * Simple array with all gtalk usernames currently being online
+     */
+    private $gtalkOnlineUsers = array();
+    
     private function fetchOnlineStatusFromIMBots()
     {
-        $facebookTutors = BotServiceProvider::createFor("http://facebook.rayku.com/tutor")->getContent();
+        $fbUsersJSON = BotServiceProvider::createFor("http://facebook.rayku.com/tutor")->getContent();
+        $fbUsers = json_decode($fbUsersJSON, true);
+        
+        foreach ($fbUsers as $fbUser) {
+            if (trim($fbUser['username']) != '') {
+                $this->fbOnlineUsers[] = trim($fbUser['username']);
+            }
+        }
+        
         $onlineTutorsByNotificationBot = BotServiceProvider::createFor("http://notification-bot.rayku.com/tutor")->getContent();
-        $gtalkTutors = BotServiceProvider::createFor("http://www.rayku.com:8892/onlines")->getContent();
-
-        $this->fbUsers = json_decode($facebookTutors, true);
-        $this->botUsers = json_decode($onlineTutorsByNotificationBot, true);
+        $this->botOnlineUsers = json_decode($onlineTutorsByNotificationBot, true);
         
-        $gtalkUsers = json_decode($gtalkTutors, true);
+        $gtalkUsersJSON = BotServiceProvider::createFor("http://www.rayku.com:8892/onlines")->getContent();
+        $gtalkUsers = json_decode($gtalkUsersJSON, true);
 
-        $this->gtalkUsers = array();
-        
         foreach ($gtalkUsers as $gtalkUserId => $status) {
             $parts = explode('/', $gtalkUserId);
-            $this->gtalkUsers[] = $parts[0];
+            if (trim($parts[0]) != '') {
+                $this->gtalkOnlineUsers[] = trim($parts[0]);
+            }
+        }
+    }
+    
+    /**
+     * prefetches all gtalk and fb IDS 
+     */
+    private function fetchCCIds()
+    {
+        $c = new Criteria;
+        $result = UserGtalkPeer::doSelectStmt($c);
+        while($row = $result->fetch(PDO::FETCH_ASSOC)) {
+            $this->gtalkIds[$row['USERID']] = $row['GTALKID'];
+        }
+        
+        $query = "SELECT * FROM user_fb";
+        $result = Propel::getConnection()->query($query);
+        while($row = $result->fetch(PDO::FETCH_ASSOC)) {
+            $this->fbIds[$row['userid']] = $row['fb_username'];
         }
     }
     
     function isGtalkOnline(User $user)
     {
-        $gtalk = $user->getUserGtalk();
-        if ($gtalk && $gtalk->getGtalkid() != '') {
-            return in_array($gtalk->getGtalkid(), $this->gtalkUsers);
+        if (!isset($this->gtalkIds[$user->getId()])) {
+            return false;
         }
+        
+        return in_array($this->gtalkIds[$user->getId()], $this->gtalkOnlineUsers);
     }
     
     private function isFBOnline(User $user)
     {
-        if (is_array($this->fbUsers)) {
-            $fb_query = mysql_query("select * from user_fb where userid=" . $user->getId()) or die(mysql_error());
-
-            if (mysql_num_rows($fb_query) > 0) {
-
-                $fbRow = mysql_fetch_assoc($fb_query);
-                $fb_username = $fbRow['fb_username'];
-
-                foreach ($this->fbUsers as $fbUser) {
-                    if ($fbUser['username'] == $fb_username) {
-                        return true;
-                    }
-                }
-            }
+        if (!isset($this->fbIds[$user->getId()])) {
+            return false;
         }
+        
+        return in_array($this->fbIds[$user->getId()], $this->fbOnlineUsers);
     }
     
     private function isMACOnline(User $user)
     {
-        if (is_array($this->botUsers)) {
-            foreach ($this->botUsers as $botUser) {
+        if (is_array($this->botOnlineUsers)) {
+            foreach ($this->botOnlineUsers as $botUser) {
                 if ($botUser['email'] == $user->getEmail()) {
                     return true;
                 }
@@ -74,16 +109,15 @@ class UsersAvailabilityChecker
         );
     
         $c = new Criteria();
-        $c->addJoin(UserPeer::ID, UserGtalkPeer::USERID, Criteria::LEFT_JOIN);
         $allUsers = UserPeer::doSelect($c);
 
         $onlineusers = array();
         
         $this->fetchOnlineStatusFromIMBots();
+        $this->fetchCCIds();
 
         /* @var $user User */
         foreach ($allUsers as $user) {
-
             if ($user->isOnline()) {
                 $this->counts['web']++;
             } else if($this->isGtalkOnline($user)) {
