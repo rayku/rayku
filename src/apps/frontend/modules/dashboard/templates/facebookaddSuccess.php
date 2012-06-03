534f91708318816ea078dfe424870c4233cc6fe2
Łukasz Wojciechowski <lukasz.wojciechowski@ngsoft.pl>
2012-06-03 15:02:26 +0200

    last touches in dashbard/facebookadd

--- src/apps/frontend/modules/dashboard/templates/facebookaddSuccess.php
+++ src/apps/frontend/modules/dashboard/templates/facebookaddSuccess.php
@@ -7,13 +7,13 @@
     <div class="content">
       <div class="entry">
         <div class="spacer"></div>
-         <?php if($display == 1) { ?>
+         <?php if($action == 1) { ?>
 
        	<div style="border-top: 2px solid rgb(56, 109, 37);  padding: 12px; background: rgb(201, 242, 201) none repeat scroll 0% 0%; color: rgb(51, 51, 51); -moz-background-clip: border; -moz-background-origin: padding; -moz-background-inline-policy: continuous; font-size: 14px; line-height: 18px; margin-bottom: 20px;">Your friend request to 'raykubot' has just been sent! We will automatically accept it in a few moments, and you will start getting notifications through Facebook.</div>
 
 	<?php } else { ?>
 
-  		 <div style="border-top:2px solid #900;padding:12px;color:#333;background:#FFF0F0;font-size:14px;line-height:18px;margin-bottom:20px;"> Opps! error. Something Wrong Here!!!</div>
+  		 <div style="border-top:2px solid #900;padding:12px;color:#333;background:#FFF0F0;font-size:14px;line-height:18px;margin-bottom:20px;"> Opps! error. Something Wrong Here!!!<br />Maybe RaykuBit was already on your FB friends list ?</div>
 
 	<?php } ?>
 <br/><br/>
