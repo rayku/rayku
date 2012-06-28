69fbaa1d59602282f16588263bb3ea6d37f0fac5
Łukasz Wojciechowski <lukasz.wojciechowski@ngsoft.pl>
2012-06-28 15:17:37 +0200

    [API] added first service for checking email/password validity

--- src/apps/api/modules/auth/actions/actions.class.php
+++ src/apps/api/modules/auth/actions/actions.class.php
@@ -1,22 +1,22 @@
 <?php
 
 /**
- * auth actions.
- *
- * @package    elifes
- * @subpackage auth
- * @author     Your name here
- * @version    SVN: $Id: actions.class.php 12479 2008-10-31 10:54:40Z fabien $
+ * Auth related API serivces
  */
 class authActions extends sfActions
 {
- /**
-  * Executes index action
-  *
-  * @param sfRequest $request A request object
-  */
-  public function executeIndex(sfWebRequest $request)
-  {
-    $this->forward('default', 'module');
-  }
+    
+    /**
+     * Simple check if given username/password are correct
+     */
+    public function executeCheckLogin(sfWebRequest $request)
+    {
+        if (!$request->hasParameter('email') || !$request->hasParameter('password')) {
+            return $this->renderText('FAIL');
+        }
+        
+        $user = UserPeer::checkLogin($request->getParameter('email'), $request->getParameter('password'));
+        
+        return $this->renderText(($user instanceof User) ? 'OK' : 'FAIL');
+    }
 }
