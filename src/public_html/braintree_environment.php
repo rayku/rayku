63c3277f537b1b9da683a8a52cd4af1407cb05b3
Miodrag Tokić <mtokic@gmail.com>
2012-12-05 00:44:41 +0100

    Removed Braintree library from public_html directory

--- src/public_html/braintree_environment.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once('braintree/lib/Braintree.php');
-
-Braintree_Configuration::environment('sandbox');
-Braintree_Configuration::merchantId('sb744r5j9242tw7x');
-Braintree_Configuration::publicKey('ps79cw885xbf6hfv');
-Braintree_Configuration::privateKey('6fn68d69szc68zx3');
-?>
