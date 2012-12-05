7f292ae9cfbc97a2db3cd1c2bfaeb220aa221e9e
Miodrag Tokić <mtokic@gmail.com>
2012-12-05 01:44:32 +0100

    Added some autoloading to the PHPUnit bootstrap

--- tests/unit/bootstrap.php
+++ tests/unit/bootstrap.php
@@ -1,4 +1,11 @@
 <?php
+
+// Project root
+define('PROJROOT', realpath(dirname(__FILE__) . '/../../src/') . '/');
+
+// Phake auto loading needs some cuddling
+set_include_path(get_include_path() . PATH_SEPARATOR . realpath(__DIR__ . '/../../src/lib/vendor/phake/src'));
+
 require_once dirname(__FILE__) . '/../../src/lib/vendor/phake/src/Phake.php';
 require_once dirname(__FILE__) . '/../../src/lib/vendor/symfony1/lib/autoload/sfCoreAutoload.class.php';
 require_once dirname(__FILE__) . '/../../src/config/ProjectConfiguration.class.php';
@@ -13,8 +20,19 @@ function mock($className)
     return Phake::mock($className);
 }
 
-$autoloader = sfCoreAutoload::getInstance();
-$autoloader->register();
+// This has to be done, so proper environment config files gets loaded
+$configuration = ProjectConfiguration::getApplicationConfiguration('frontend', 'test', true);
+sfContext::createInstance($configuration);
+unset($configuration);
+
+$coreAutoload = sfCoreAutoload::getInstance();
+$coreAutoload->register();
+
+$simpleAutoload = sfSimpleAutoload::getInstance();
+$simpleAutoload->addDirectory(realpath(__DIR__ . '/../../src/lib/vendor'));
+$simpleAutoload->addDirectory(realpath(__DIR__ . '/../../src/apps/frontend/lib'));
+$simpleAutoload->addDirectory(realpath(__DIR__ . '/../../src/lib/model'));
+$simpleAutoload->register();
 
 
 class SymfonyActionTestCase extends PHPUnit_Framework_TestCase
