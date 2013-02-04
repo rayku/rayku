80a19d396bd1f0d011e556658dce8854a4001361
Colin Hutchinson <colin@hutchic.com>
2013-02-04 11:02:27 -0500

    Synced the quick reg / registratin emails

--- src/apps/frontend/modules/quickreg/templates/_activationEmail.php
+++ src/apps/frontend/modules/quickreg/templates/_activationEmail.php
@@ -1,7 +1,8 @@
-<p>Hi <?php echo $user->getName(); ?>
-<br /><br />
-Congrats! You are almost ready to start using rayku.com!<br /><br />
-As a final step, please confirm your email by clicking on the link below:<br/><br/>
-<a href="<?php echo sfConfig::get('app_rayku_url') ?>/register/confirm/<?php echo $confirmationCode; ?>/temp"><?php echo sfConfig::get('app_rayku_url') ?>/register/confirm/<?php echo $confirmationCode; ?>/temp</a><br /><br />
-Thanks!<br />
-Rayku Administration</p>
+Hey <?php echo $user->getName(); ?><br /><br />
+
+Thanks for registering at Rayku! Please confirm your email address in order to activate your account.<br /><br />
+
+To do that, simply go to the following link:<br />
+<?php echo $activationLink; ?>
+
+<?php include_partial( 'global/emailFooter' ); ?>
