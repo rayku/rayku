2b400aa781691734c460793b2746be1f0f4b92f6
Colin Hutchinson <colin@hutchic.com>
2013-02-20 14:49:05 -0500

    Tutor list database connection

--- src/lib/task/tutorListTask.class.php
+++ src/lib/task/tutorListTask.class.php
@@ -4,29 +4,29 @@ class tutorListTask extends sfBaseTask {
 
     public function configure()
     {
-        $this->addOptions(array(new sfCommandOption('connection', null, sfCommandOption::PARAMETER_REQUIRED, 'connection name'),));
-
-
+    	$this->addOptions(array(
+    			new sfCommandOption('application', null, sfCommandOption::PARAMETER_REQUIRED, 'The application name', 'frontend'),
+    			new sfCommandOption('env', null, sfCommandOption::PARAMETER_REQUIRED, 'The environment', 'dev'),
+    	));
+    	
         $this->namespace = 'tutor';
         $this->name      = 'list';
     }
 
     protected function execute($arguments = array(), $options = array()) {
-
         $databaseManager = new sfDatabaseManager($this->configuration);
-        $connection = $databaseManager->getDatabase($options['connection'] ? $options['connection'] : null)->getConnection();
+        $connection = $databaseManager->getDatabase('propel')->getConnection();
 
-        /*$statusFinder=new UsersAvailabilityChecker();
-        $onlineUsers=$statusFinder->getOnlineUsers();*/
-        mysql_query("SELECT * FROM user")or die('Dead');
+        $statusFinder=new UsersAvailabilityChecker();
+        $onlineUsers=$statusFinder->getOnlineUsers();
 
-        /*foreach($onlineUsers as $onlineUser){
+        foreach($onlineUsers as $onlineUser){
             if(mysql_num_rows(mysql_query("SELECT * FROM tutor_profile WHERE user_id='$onlineUser'"))>0){
                 if(mysql_query("UPDATE tutor_profile SET online_status='0' WHERE user_id='$onlineUser'")){
                     $this->log('Hello, World!');
                 };
             }
-        }*/
+        }
     }
 
 }
