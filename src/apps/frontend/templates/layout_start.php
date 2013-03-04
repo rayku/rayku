1ee8c5aa64fe0083bca6d7498aaa0132e80e034e
Colin Hutchinson <colin@hutchic.com>
2013-03-04 10:31:49 -0500

    Quick Registration Setup

--- src/apps/frontend/templates/layout_start.php
+++ src/apps/frontend/templates/layout_start.php
@@ -2,160 +2,233 @@
 <html lang="en">
 <head>
 <meta charset="utf-8" />
-<title>Online Math Tutoring | Calculus, Algebra, Statistics, more | Rayku.com</title>
-<link href="<?php echo $baseRootPath; ?>/landing/css/style.css" type="text/css" rel="stylesheet" media="screen" />
-<link href="http://fonts.googleapis.com/css?family=Open+Sans:400,700,600" rel="stylesheet" type="text/css" />
+<title>Online Math Tutoring | Calculus, Algebra, Statistics, more |
+	Rayku.com</title>
+<link href="<?php echo $baseRootPath; ?>/landing/css/style.css"
+	type="text/css" rel="stylesheet" media="screen" />
+<link
+	href="http://fonts.googleapis.com/css?family=Open+Sans:400,700,600"
+	rel="stylesheet" type="text/css" />
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
 <script src="<?php echo $baseRootPath; ?>/landing/js/global.js"></script>
 </head>
 
 <body class="home">
-<header>
-  <div class="inside"> <a href="<?php echo $baseRootPath; ?>" id="logo"><img src="<?php echo $baseRootPath; ?>/landing/images/logo.png" alt="" /></a>
-    <nav> <a href="<?php echo $baseRootPath; ?>/login">Login</a> <a href="<?php echo $baseRootPath; ?>/register">Register</a> <a href="<?php echo $baseRootPath; ?>/joinus">Become a Tutor</a> </nav>
-  </div>
-</header>
-<div id="intro" class="v1">
-  <div class="inside"> 
-    <!--[if IE]>
+	<header>
+		<div class="inside">
+			<a href="<?php echo $baseRootPath; ?>" id="logo"><img
+				src="<?php echo $baseRootPath; ?>/landing/images/logo.png" alt="" />
+			</a>
+			<nav>
+				<a href="<?php echo $baseRootPath; ?>/login">Login</a> <a
+					href="<?php echo $baseRootPath; ?>/register">Register</a> <a
+					href="<?php echo $baseRootPath; ?>/joinus">Become a Tutor</a>
+			</nav>
+		</div>
+	</header>
+	<div id="intro" class="v1">
+		<div class="inside">
+			<!--[if IE]>
 <div style="width:100%;padding:8px 0;background:#FFCCCC;border-bottom:2px solid #BF3535;font-size:14px;color:#666" text-align="center">
 Rayku currently does not work every well with <strong>Internet Explorer</strong>. Please use Firefox or Chrome or another browser.
 </div>
 <![endif]-->
-    <div class="left">
-      <h1>Instant Math Tutoring</h1>
-      <h2>Connect with a live online tutor, over a beautiful &amp; interactive whiteboard, whenever you need help. Your first session is free!</h2>
-      <form action="<?php echo $baseRootPath; ?>/register">
-        <p>
-          <input type="text" class="ask" placeholder="Describe what you need help with..." />
-          <input type="submit" value="Ask" />
-        </p>
-        <div id="signup-ask">
-          <div class="head">Please provide account information on order to continue <a href="<?php echo $baseRootPath; ?>/login" class="right">Already have an account</a>
-            <div class="clear"></div>
-          </div>
-          <ul>
-            <li>
-              <input type="text" placeholder="Full Name" required />
-            </li>
-            <li>
-              <input type="email" placeholder="E-mail Address" required />
-            </li>
-            <li>
-              <input type="password" placeholder="Password" required />
-            </li>
-            <li>
-              <input type="password" placeholder="Confirm Password" required />
-            </li>
-            <li class="terms">By clicking Create Account you are indicating that you have read and agree to the <strong>Terms & Conditions</strong></li>
-            <li class="submit">
-              <input type="submit" value="Create an account and ask" />
-            </li>
-          </ul>
-        </div>
-      </form>
-      <p class="seen-on"> As seen on: <img src="<?php echo $baseRootPath; ?>/landing/images/seen_on.png" alt="" /> </p>
-    </div>
-    <div class="right"> <a href="http://fast.wistia.net/embed/iframe/u4g1ocbuke?autoPlay=true&plugin%5BpostRoll-v1%5D%5Blink%5D=http%3A%2F%2Frayku.com%2Fregister&plugin%5BpostRoll-v1%5D%5Bstyle%5D%5BbackgroundColor%5D=%23333333&plugin%5BpostRoll-v1%5D%5Bstyle%5D%5Bcolor%5D=%23ddeefc&plugin%5BpostRoll-v1%5D%5Bstyle%5D%5BfontFamily%5D=Gill%20Sans%2C%20Helvetica%2C%20Arial%2C%20sans-serif&plugin%5BpostRoll-v1%5D%5Bstyle%5D%5BfontSize%5D=36px&plugin%5BpostRoll-v1%5D%5Btext%5D=Click%20Here%3Cbr%2F%3Eto%20Get%20Started%3Cbr%2F%3E%28it%27s%20free%21%29&popover=true&smallPlayButton=false&version=v1&videoHeight=360&videoWidth=640&volumeControl=true" class="wistia-popover[height=386,width=640] video"><img src="<?php echo $baseRootPath; ?>/landing/images/video.jpg" alt="" /> <span class="play"></span></a> 
-      <script charset="ISO-8859-1" src="http://fast.wistia.com/static/popover-v1.js"></script> 
-    </div>
-    <div class="clear"></div>
-  </div>
-  <div class="wood">
-    <div class="dust left"></div>
-    <div class="dust"></div>
-    <div class="chalk"></div>
-    <div class="dust right"></div>
-  </div>
-</div>
-<div id="steps">
-  <div class="inside">
-    <div class="step">
-      <div class="text">
-        <h3>Ask Question</h3>
-        <p>Let us know the  question or topic that you need help with.</p>
-      </div>
-      <div class="img"></div>
-    </div>
-    <div class="step">
-      <div class="text">
-        <h3>Select Tutor</h3>
-        <p>Select from a list of online tutors that are BEST suited to help you.</p>
-      </div>
-      <div class="img"></div>
-    </div>
-    <div class="step">
-      <div class="text">
-        <h3>Learn!</h3>
-        <p>Connect with a live tutor over our whiteboard, within 60 seconds!</p>
-      </div>
-      <div class="img"></div>
-    </div>
-    <div class="clear"></div>
-  </div>
-</div>
-<div id="testimonials">
-  <div class="inside">
-    <hgroup>
-      <h2>People love Rayku</h2>
-      <h3>Find out what everyone is saying about us. People are talking!</h3>
-    </hgroup>
-  </div>
-  <div class="testimonials">
-    <div class="testimonial element">
-      <blockquote>
-        <p>My son got in touch with a Yale math major who helped him understand a math problem that neither of us could figure out. This is a crucial year for him, and Rayku.com is one way for me to support his efforts.</p>
-        <footer>Sarah Ming <em>Parent, Kingston New York</em></footer>
-      </blockquote>
-      <div class="avatar"> <img src="<?php echo $baseRootPath; ?>/landing/images/sarah-icon.png" alt="" /> </div>
-    </div>
-    <div class="testimonial element">
-      <blockquote>
-        <p>With the new whiteboard, it's fun and super simple to use. Rayku is a great platform to assist others that are now in a position similar to where I once was.</p>
-        <footer>Paul Morgan <em>Rayku Tutor, MIT Student</em></footer>
-      </blockquote>
-      <div class="avatar"> <img src="<?php echo $baseRootPath; ?>/landing/images/paul-icon.png" alt="" /> </div>
-    </div>
-    <div class="testimonial element">
-      <blockquote>
-        <p>Available globally 24 hours a day, seven days a week, Rayku enables tutors to set their own fees, and students to select their tutors based on price and quality. The Canadian startup estimates that this saves users an average of 60% per tutoring session compared with conventional alternatives.</p>
-        <footer>Techvibes <em>Technology News Blog</em></footer>
-      </blockquote>
-      <div class="avatar"> <img src="<?php echo $baseRootPath; ?>/landing/images/tv-icon.png" alt="" /> </div>
-    </div>
-  </div>
-</div>
-<div id="whiteboard">
-  <div class="inside">
-    <hgroup>
-      <h2>Rayku works because we show, not tell</h2>
-      <h3>We've put together a fast and easy-to-use whiteboard app, with everything from audio/video to a sleek math equation writer. Connect with your online tutor immediately for a painless experience.</h3>
-    </hgroup>
-    <div class="macbook"> <img src="<?php echo $baseRootPath; ?>/landing/images/whiteboard_demo.jpg" alt="" />
-      <ul>
-        <li> <strong>Tools</strong> The math equation writer makes it easy to communicate your question and thoughts </li>
-        <li> <strong>Board</strong> Watch as the tutor illustrates each step in a visual way on the live shared whiteboard </li>
-        <li> <strong>Video</strong> See or listen to the tutor explain the concepts in question as they draw it out for you in real time </li>
-        <li> <strong>Chat</strong> More comfortable typing? We've got you covered! Everything is recorded for you as notes</li>
-      </ul>
-    </div>
-  </div>
-</div>
-<div id="signup">
-  <div class="inside">
-    <hgroup>
-      <h2>Get your first session free!</h2>
-      <h3>Create a free account to get started (no credit card required):</h3>
-    </hgroup>
-    <p> <a href="<?php echo $baseRootPath; ?>/register" class="signup">Register Now</a> </p>
-  </div>
-</div>
-<footer class="footer">
-  <div class="inside">
-    <p class="copy left">Copyright 2013 Rayku Corp. All rights reserved | Questions? 1-888-98RAYKU</p>
-    <nav class="right"> <a href="<?php echo $baseRootPath; ?>/about">About</a> <a href="<?php echo $baseRootPath; ?>/jobs">Jobs</a> <a href="<?php echo $baseRootPath; ?>/about#contact">Contact Us</a> <a href="<?php echo $baseRootPath; ?>/tos.html">Legal</a> </nav>
-    <div class="clear"></div>
-  </div>
-</footer>
+			<div class="left">
+				<h1>Instant Math Tutoring</h1>
+				<h2>Connect with a live online tutor, over a beautiful &amp;
+					interactive whiteboard, whenever you need help. Your first session
+					is free!</h2>
+				<form action="<?php echo $baseRootPath; ?>/register" method="post">
+					<p>
+						<input type="text" class="ask"
+							placeholder="Describe what you need help with..." /> <input
+							type="submit" value="Ask" />
+					</p>
+					<div id="signup-ask">
+						<div class="head">
+							Please provide account information on order to continue <a
+								href="<?php echo $baseRootPath; ?>/login" class="right">Already
+								have an account</a>
+							<div class="clear"></div>
+						</div>
+						<ul>
+							<li><input type="text" name="realname" placeholder="Full Name" required />
+							</li>
+							<li><input type="email" name="email" placeholder="E-mail Address" required />
+							</li>
+							<li><input type="password" name="password1" placeholder="Password" required />
+							</li>
+							<li><input type="password" placeholder="Confirm Password"
+								required />
+							</li>
+							<li class="terms">By clicking Create Account you are indicating
+								that you have read and agree to the <strong>Terms & Conditions</strong>
+							</li>
+							<li class="submit"><input type="submit"
+								value="Create an account and ask" />
+							</li>
+						</ul>
+					</div>
+				</form>
+				<p class="seen-on">
+					As seen on: <img
+						src="<?php echo $baseRootPath; ?>/landing/images/seen_on.png"
+						alt="" />
+				</p>
+			</div>
+			<div class="right">
+				<a
+					href="http://fast.wistia.net/embed/iframe/u4g1ocbuke?autoPlay=true&plugin%5BpostRoll-v1%5D%5Blink%5D=http%3A%2F%2Frayku.com%2Fregister&plugin%5BpostRoll-v1%5D%5Bstyle%5D%5BbackgroundColor%5D=%23333333&plugin%5BpostRoll-v1%5D%5Bstyle%5D%5Bcolor%5D=%23ddeefc&plugin%5BpostRoll-v1%5D%5Bstyle%5D%5BfontFamily%5D=Gill%20Sans%2C%20Helvetica%2C%20Arial%2C%20sans-serif&plugin%5BpostRoll-v1%5D%5Bstyle%5D%5BfontSize%5D=36px&plugin%5BpostRoll-v1%5D%5Btext%5D=Click%20Here%3Cbr%2F%3Eto%20Get%20Started%3Cbr%2F%3E%28it%27s%20free%21%29&popover=true&smallPlayButton=false&version=v1&videoHeight=360&videoWidth=640&volumeControl=true"
+					class="wistia-popover[height=386,width=640] video"><img
+					src="<?php echo $baseRootPath; ?>/landing/images/video.jpg" alt="" />
+					<span class="play"></span> </a>
+				<script charset="ISO-8859-1"
+					src="http://fast.wistia.com/static/popover-v1.js"></script>
+			</div>
+			<div class="clear"></div>
+		</div>
+		<div class="wood">
+			<div class="dust left"></div>
+			<div class="dust"></div>
+			<div class="chalk"></div>
+			<div class="dust right"></div>
+		</div>
+	</div>
+	<div id="steps">
+		<div class="inside">
+			<div class="step">
+				<div class="text">
+					<h3>Ask Question</h3>
+					<p>Let us know the question or topic that you need help with.</p>
+				</div>
+				<div class="img"></div>
+			</div>
+			<div class="step">
+				<div class="text">
+					<h3>Select Tutor</h3>
+					<p>Select from a list of online tutors that are BEST suited to help
+						you.</p>
+				</div>
+				<div class="img"></div>
+			</div>
+			<div class="step">
+				<div class="text">
+					<h3>Learn!</h3>
+					<p>Connect with a live tutor over our whiteboard, within 60
+						seconds!</p>
+				</div>
+				<div class="img"></div>
+			</div>
+			<div class="clear"></div>
+		</div>
+	</div>
+	<div id="testimonials">
+		<div class="inside">
+			<hgroup>
+				<h2>People love Rayku</h2>
+				<h3>Find out what everyone is saying about us. People are talking!</h3>
+			</hgroup>
+		</div>
+		<div class="testimonials">
+			<div class="testimonial element">
+				<blockquote>
+					<p>My son got in touch with a Yale math major who helped him
+						understand a math problem that neither of us could figure out.
+						This is a crucial year for him, and Rayku.com is one way for me to
+						support his efforts.</p>
+					<footer>
+						Sarah Ming <em>Parent, Kingston New York</em>
+					</footer>
+				</blockquote>
+				<div class="avatar">
+					<img
+						src="<?php echo $baseRootPath; ?>/landing/images/sarah-icon.png"
+						alt="" />
+				</div>
+			</div>
+			<div class="testimonial element">
+				<blockquote>
+					<p>With the new whiteboard, it's fun and super simple to use. Rayku
+						is a great platform to assist others that are now in a position
+						similar to where I once was.</p>
+					<footer>
+						Paul Morgan <em>Rayku Tutor, MIT Student</em>
+					</footer>
+				</blockquote>
+				<div class="avatar">
+					<img
+						src="<?php echo $baseRootPath; ?>/landing/images/paul-icon.png"
+						alt="" />
+				</div>
+			</div>
+			<div class="testimonial element">
+				<blockquote>
+					<p>Available globally 24 hours a day, seven days a week, Rayku
+						enables tutors to set their own fees, and students to select their
+						tutors based on price and quality. The Canadian startup estimates
+						that this saves users an average of 60% per tutoring session
+						compared with conventional alternatives.</p>
+					<footer>
+						Techvibes <em>Technology News Blog</em>
+					</footer>
+				</blockquote>
+				<div class="avatar">
+					<img src="<?php echo $baseRootPath; ?>/landing/images/tv-icon.png"
+						alt="" />
+				</div>
+			</div>
+		</div>
+	</div>
+	<div id="whiteboard">
+		<div class="inside">
+			<hgroup>
+				<h2>Rayku works because we show, not tell</h2>
+				<h3>We've put together a fast and easy-to-use whiteboard app, with
+					everything from audio/video to a sleek math equation writer.
+					Connect with your online tutor immediately for a painless
+					experience.</h3>
+			</hgroup>
+			<div class="macbook">
+				<img
+					src="<?php echo $baseRootPath; ?>/landing/images/whiteboard_demo.jpg"
+					alt="" />
+				<ul>
+					<li><strong>Tools</strong> The math equation writer makes it easy
+						to communicate your question and thoughts</li>
+					<li><strong>Board</strong> Watch as the tutor illustrates each step
+						in a visual way on the live shared whiteboard</li>
+					<li><strong>Video</strong> See or listen to the tutor explain the
+						concepts in question as they draw it out for you in real time</li>
+					<li><strong>Chat</strong> More comfortable typing? We've got you
+						covered! Everything is recorded for you as notes</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+	<div id="signup">
+		<div class="inside">
+			<hgroup>
+				<h2>Get your first session free!</h2>
+				<h3>Create a free account to get started (no credit card required):</h3>
+			</hgroup>
+			<p>
+				<a href="<?php echo $baseRootPath; ?>/register" class="signup">Register
+					Now</a>
+			</p>
+		</div>
+	</div>
+	<footer class="footer">
+		<div class="inside">
+			<p class="copy left">Copyright 2013 Rayku Corp. All rights reserved |
+				Questions? 1-888-98RAYKU</p>
+			<nav class="right">
+				<a href="<?php echo $baseRootPath; ?>/about">About</a> <a
+					href="<?php echo $baseRootPath; ?>/jobs">Jobs</a> <a
+					href="<?php echo $baseRootPath; ?>/about#contact">Contact Us</a> <a
+					href="<?php echo $baseRootPath; ?>/tos.html">Legal</a>
+			</nav>
+			<div class="clear"></div>
+		</div>
+	</footer>
 </body>
 </html>
